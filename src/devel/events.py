0223392542d2a845476a625d743b0f0dcba37f14
Jakub Vesely <jakub_vesely@seznam.cz>
2023-02-16 07:54:33 +0100

    added power info + enlarge measurement delay

--- src/devel/events.py
+++ src/devel/events.py
@@ -2,35 +2,18 @@
 #  This software is published under MIT license. Full text of the license is available at https://opensource.org/licenses/MIT
 
 from ___basal.___logging import Logging
+
 from ___blocks.___meteo_block import MeteoBlock
-from ___blocks.___display_block import DisplayBlock
 from ___basal.___remote_value import RemoteValue
 
 class Plan():
     def __init__(self) -> None:
         self.logging = Logging("Plan")
+
         self.meteo = MeteoBlock()
-        self.meteo.temperature.changed(self.value_changed)
-        self.meteo.pressure.changed(self.value_changed)
-        self.meteo.humidity.changed(self.value_changed)
-        self.first_values_update_listener =  self.meteo.value.updated(self.first_update)
 
-        RemoteValue.add("meteo.temperature", self.meteo.temperature)
-        RemoteValue.add("meteo.pressure", self.meteo.pressure)
-        RemoteValue.add("meteo.humidity", self.meteo.humidity)
+        RemoteValue.add("test.meteo.temperature", self.meteo.temperature)
+        RemoteValue.add("test.meteo.pressure", self.meteo.pressure)
+        RemoteValue.add("test.meteo.humidity", self.meteo.humidity)
 
-        self.display = DisplayBlock()
-        self.display.contrast(1)
-
-    def first_update(self):
-        self.meteo.value._remove_listener(self.first_values_update_listener)
-        self.value_changed()
-
-    def value_changed(self):
-        self.logging.info(str(self.meteo.temperature) + ", " + str(self.meteo.pressure) + ", " + str(self.meteo.humidity))
-        self.display.clean()
-        self.display.print_text(1, 1,  self.meteo.temperature.get_str_to_fit(7))
-        self.display.print_text(1, 10, self.meteo.pressure.get_str_to_fit(7))
-        self.display.print_text(1, 20, self.meteo.humidity.get_str_to_fit(7))
-        self.display.showtime()
 Plan()
--- src/examples/meteo/events.py
+++ src/examples/meteo/events.py
@@ -2,26 +2,36 @@
 #  This software is published under MIT license. Full text of the license is available at https://opensource.org/licenses/MIT
 
 from basal.logging import Logging
+from blocks.power_block import PowerBlock
 from blocks.meteo_block import MeteoBlock
 from basal.remote_value import RemoteValue
 from basal.active_variable import ActiveVariable
+from basal.power_mgmt import PowerMgmt, PowerPlan, BlePowerPlan
+
 class Plan():
     def __init__(self) -> None:
         self.logging = Logging("Plan")
 
-        self.meteo = MeteoBlock()
+        self.meteo = MeteoBlock(measurement_period=120)
+        self.power = PowerBlock(measurement_period=120)
 
         RemoteValue.add("test.meteo.temperature", self.meteo.temperature)
-
         RemoteValue.add("test.meteo.pressure", self.meteo.pressure)
         RemoteValue.add("test.meteo.humidity", self.meteo.humidity)
 
+        RemoteValue.add("test.power.is_usb_connected", self.power.is_usb_connected)
+        RemoteValue.add("test.power.is_charging", self.power.is_charging)
+        RemoteValue.add("test.power.battery_voltage", self.power.battery_voltage)
+        RemoteValue.add("test.power.battery_current", self.power.battery_current)
+
         self.beat_status = False
-        RemoteValue.add("test.main.status", ActiveVariable(initial_value=True, renew_period=1, renew_func=self.beats))
+        RemoteValue.add("test.main.status", ActiveVariable(initial_value=True, renew_period=120, renew_func=self.beats))
+
+        PowerMgmt.set_plan(PowerPlan(True, PowerPlan.freq_min, BlePowerPlan(90, 1, 120)))
 
     def beats(self):
         self.logging.info("beat");
         self.beat_status = not self.beat_status
-        return "1" if self.beat_status else "0"
+        return 1 if self.beat_status else 0
 
 Plan()
