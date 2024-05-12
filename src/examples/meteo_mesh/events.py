bbcdbe9d1926a5e259b770c05a930eea7687930e
Jakub Vesely <jakub_vesely@seznam.cz>
2024-05-12 08:59:31 +0200

    support for internal meteo set

--- src/examples/meteo_mesh/events.py
+++ src/examples/meteo_mesh/events.py
@@ -17,14 +17,26 @@ from quantities.current import Current
 from quantities.voltage import Voltage
 from basal.active_variable import ActiveVariable
 from basal.active_quantity import ActiveQuantity
-from blocks.block_types import BlockTypes
 from basal.power_mgmt import PowerMgmt, PowerPlan
+
+class DeviceQuantity(ActiveQuantity):
+  def __init__(self, device_id:int, quantity:str):
+    super().__init__(quantity)
+    self._device_id = device_id
+class DeviceVariable(ActiveVariable):
+  def __init__(self, device_id:int, name:str, initial_value=None):
+    super().__init__(initial_value)
+    self._name = name
+    self._device_id = device_id
+
+  def get_name(self):
+    return self._name
 
 class Plan():
   def __init__(self) -> None:
     self.logging = Logging("events")
     self.ble = BleBlock()
-    self.quantities = []
+    self.active_vars = []
 
     PowerMgmt.set_plan(PowerPlan.get_max_performance_plan())
 
@@ -33,46 +45,27 @@ class Plan():
     #reboot once a day to clean fragmented RAM
     Planner.postpone(60 * 60 * 24, mainBlock.reboot)
 
-    temperature = ActiveQuantity(Temperature("t", 3))
-    RemoteValue.add("test.ambient.temperature", temperature)
-    self.quantities.append(temperature)
+    self.add_variable(DeviceQuantity(5, Temperature("t", 3)), "in1.ambient.temperature")
+    self.add_variable(DeviceQuantity(5, Pressure("P", 3)), "in1.ambient.pressure")
+    self.add_variable(DeviceQuantity(5, RelativeHumidity("RH", 3)), "in1.ambient.humidity")
 
-    pressure = ActiveQuantity(Pressure("P", 3))
-    RemoteValue.add("test.ambient.pressure", pressure)
-    self.quantities.append(pressure)
+    self.add_variable(DeviceQuantity(5, Current("I", 3)), "in1.power.battery_current")
+    self.add_variable(DeviceQuantity(5, Voltage("U", 3)), "in1.power.battery_voltage")
 
-    humidity = ActiveQuantity(RelativeHumidity("RH", 3))
-    RemoteValue.add("test.ambient.humidity", humidity)
-    self.quantities.append(humidity)
+    self.add_variable(DeviceQuantity(3, Temperature("t", 3)), "test.ambient.temperature")
+    self.add_variable(DeviceQuantity(3, Pressure("P", 3)), "test.ambient.pressure")
+    self.add_variable(DeviceQuantity(3, RelativeHumidity("RH", 3)), "test.ambient.humidity")
 
-    wind_direction = ActiveQuantity(Angle("wd", 3))
-    RemoteValue.add("test.ambient.wind_direction", wind_direction)
-    self.quantities.append(wind_direction)
+    self.add_variable(DeviceQuantity(3, Angle("wd", 3)), "test.ambient.wind_direction")
+    self.add_variable(DeviceQuantity(3, Speed("ws", 3)), "test.ambient.wind_speed")
+    self.add_variable(DeviceQuantity(3, length("rain", 3)), "test.ambient.rain_gauge")
 
-    wind_speed = ActiveQuantity(Speed("ws", 3))
-    RemoteValue.add("test.ambient.wind_speed", wind_speed)
-    self.quantities.append(wind_speed)
+    self.add_variable(DeviceQuantity(3, Current("I", 3)), "test.power.battery_current")
+    self.add_variable(DeviceQuantity(3, Voltage("U", 3)), "test.power.battery_voltage")
 
-    rain_gauge = ActiveQuantity(length("rain", 3))
-    RemoteValue.add("test.ambient.rain_gauge", rain_gauge)
-    self.quantities.append(rain_gauge)
-
-    current = ActiveQuantity(Current("I", 3))
-    RemoteValue.add("test.power.battery_current", current)
-    self.quantities.append(current)
-
-    voltage = ActiveQuantity(Voltage("U", 3))
-    RemoteValue.add("test.power.battery_voltage", voltage)
-    self.quantities.append(voltage)
-
-    self.is_usb_connected = ActiveVariable(False)
-    self.is_charging = ActiveVariable(False)
-    self.heartbeat = ActiveVariable(False)
-
-    RemoteValue.add("test.power.is_usb_connected", self.is_usb_connected)
-    RemoteValue.add("test.power.is_charging", self.is_charging)
-
-    RemoteValue.add("test.main.status", self.heartbeat)
+    self.add_variable(DeviceVariable(3, "usb", False), "test.power.is_usb_connected")
+    self.add_variable(DeviceVariable(3, "charg", False), "test.power.is_charging")
+    self.add_variable(DeviceVariable(3, "heart", False), "test.main.status")
 
     #from blocks.rj12 import Rj12Block
     #rj_block = Rj12Block(0x30)
@@ -95,23 +88,28 @@ class Plan():
     #Planner.postpone(1, self.write)
 
 
-  def write(self):
-    #self.ble.write_at_command(b"AT\x0d\x0a")
-    #self.ble.write_at_command("AT+NAMEJDY-03")
-    #self.ble.write_at_command("AT+ROLE5")
-    #self.ble.write_at_command("AT+MCLSS0")
-    #self.ble.write_at_command("AT+RESET")
-    #self.ble.write_at_command("AT+SLEEP2")
-    #self.ble.write_at_command("W")
-    #self.ble.write_at_command("AT+NETID1122")
-    #self.ble.write_at_command(b"AT+MADDR\x0d\x0a")
-    #self.ble.write_at_command("AT+MADDR0002")
-    #self.ble.write_at_command(bytes.fromhex("41542b4d45534801000241430d0a"))
-    self.ble.write_at_command(b"AT+MESH\x01\x00\x03I=200 mA\x0d\x0a\x0d\x0a")
+  def add_variable(self, quantity, remote_id):
+    RemoteValue.add(remote_id, quantity)
+    self.active_vars.append(quantity)
 
-  def send_mesh(self):
+  #def write(self):
+    #self.ble.write_at_command(b"AT") #communication test
+    #self.ble.write_at_command("AT+NAMEJDY-04") #set name
+    #self.ble.write_at_command("AT+ROLE5") #set role
+    #self.ble.write_at_command("AT+MCLSS1") #0:router, 1: terminal
+    #self.ble.write_at_command("AT+RESET")
+    #self.ble.write_at_command("AT+SLEEP2") #1: light, 2:deep
+    #self.ble.write_at_command("W")
+    #self.ble.write_at_command("AT+NETID1122") #id of network
+    #self.ble.write_at_command(b"AT+MADDR\x0d\x0a") # get addr
+    #self.ble.write_at_command("AT+MADDR0005") #set addr
+    #self.ble.write_at_command(bytes.fromhex("41542b4d45534801000241430d0a"))
+
+    #self.ble.write_at_command(b"AT+MESH\x01\x00\x03I=200 mA\x0d\x0a\x0d\x0a")
+
+  #def send_mesh(self):
      #self.ble.write_at_command(b"AT+MADDR\x0d\x0a")
-     self.ble.write_at_command(b"AT+MESH\x00\x00\x03I=200 mA\x0d\x0a")
+     #self.ble.write_at_command(b"AT+MESH\x00\x00\x03I=200 mA\x0d\x0a")
      #resp = self.ble.send_mesh_message(0x03, b":-)")
      #self.logging.info(resp)
 
@@ -132,27 +130,22 @@ class Plan():
       data = self.ble.read_mesh_message()
       if not data:
         break;
+      #self.logging.info(data)
+
       device_id = data[0];
       message:str = data[1:].decode('ascii')
       parts = message.split("=")
-      quantity_name = parts[0]
+      var_name = parts[0]
       parts = parts[1].split(" ")
       value = float(parts[0])
       units = parts[1] if len(parts) > 1 else ""
 
-      if device_id == 3:
-        quantity: ActiveQuantity
-        for quantity in self.quantities:
-          if quantity.get_name() == quantity_name:
-            quantity.set_with_units(value, units)
-            return
-        if quantity_name == "usb":
-          self.is_usb_connected.set(value)
-        elif quantity_name == "charg":
-          self.is_charging.set(value)
-        elif quantity_name == "heart":
-          self.heartbeat.set(value)
-        else:
-          self.logging.value(data)
+      for variable in self.active_vars:
+        if variable._device_id == device_id and variable.get_name() == var_name:
+          if isinstance(variable, DeviceQuantity):
+            variable.set_with_units(value, units)
+          else:
+            variable.set(value)
+      #self.logging.value(data)
 
 Plan()
